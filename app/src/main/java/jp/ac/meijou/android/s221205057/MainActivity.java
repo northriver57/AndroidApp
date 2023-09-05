1ca94607cfa583ac37a6a59a986c5063a4954b67
northriver57 <115207536+northriver57@users.noreply.github.com>
2023-09-05 13:40:46 +0900

    テキストボックスに文字を入力するとテキストビューに即時反映されるようにしました．

--- app/src/main/java/jp/ac/meijou/android/s221205057/MainActivity.java
+++ app/src/main/java/jp/ac/meijou/android/s221205057/MainActivity.java
@@ -3,12 +3,45 @@ package jp.ac.meijou.android.s221205057;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.TextView;
+
+import jp.ac.meijou.android.s221205057.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.button.setOnClickListener(view ->{
+            var text = binding.editTextText.getText().toString();
+            binding.text.setText(text);
+
+        });
+        binding.editTextText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                binding.text.setText(editable.toString());
+            }
+        });
+
+
     }
 }

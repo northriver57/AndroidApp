015e231c65a88c878ac5fea6138d81389242050e
northriver57 <115207536+northriver57@users.noreply.github.com>
2023-09-07 15:03:11 +0900

    4日目のプログラム ネットワーク経由での画像取得，ネットワーク情報の取得

---
+++ app/src/main/java/jp/ac/meijou/android/s221205057/Gist.java
@@ -0,0 +1,12 @@
+package jp.ac.meijou.android.s221205057;
+
+import java.util.Map;
+
+public class Gist {
+
+    public Map<String, Gistfile> files;
+
+    public static class Gistfile{
+        public String content;
+    }
+}
